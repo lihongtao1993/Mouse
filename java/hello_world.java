84421a3a6dac0d971f36dbaedb93585b982c05d4
lihongtao1993 <1021252402@qq.com>
2018-10-28 00:53:26 +0800

    add add(a,b)

--- java/hello_world.java
+++ java/hello_world.java
@@ -1,5 +1,12 @@
 public class hello_world{
 	public static void main (String[] args){
 		System.out.println("Hello world");
+		add(5,7);
+	}
+	public static void add(int a,int b){
+		//int a,b
+		int c;
+		c=a+b;
+		System.out.println(a+"+"+b+"="+c);	
 	}
 }
